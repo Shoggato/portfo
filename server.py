007799682e50ccddcb916d03f276559633155a91
Erika <eeels1986@gmail.com>
2024-01-03 18:30:37 -0600

    added requirements.txt

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/index.html')
+@app.route('/home')
 def my_home():
     return render_template('index.html')
 
@@ -11,6 +11,3 @@ def my_home():
 def download_my_cv():
     path = 'static/files/ErikaWalkerResume.pdf'
     return send_file(path, as_attachment=True)
-
-if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='127.0.0.1')
